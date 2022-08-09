ebabb14724dd5aef624a685b1e27081d4a39009b
Anatolii <anatolii.klots@gmail.com>
2022-08-10 00:22:23 +0200

    almost done

--- m5stack-sbus-remote/src/main.cpp
+++ m5stack-sbus-remote/src/main.cpp
@@ -1,7 +1,13 @@
 #include "sbus.h"
-#include <M5Unified.h>
-
+#include <M5Stack.h>
+#include <Wire.h>
 #include <esp_log.h>
+
+#include "lego_plus_driver.h"
+
+#define GFXFF 1
+#define FF18  &FreeSans12pt7b
+#define CF_OL24 &Orbitron_Light_24
 
 bfs::SbusRx sbus_rx(&Serial1);
 std::array<int16_t, bfs::SbusRx::NUM_CH()> sbus_data;
@@ -11,37 +17,43 @@ const int max_ch_value = 1800;
 
 const int max_ch = 8;
 
+TFT_eSprite spr = TFT_eSprite(&M5.Lcd);
+
 int16_t perc(int16_t value)
 {
   return map(value, min_ch_value, max_ch_value, 0, 100);
 }
 
 void setup() {
+  M5.begin();
+  M5.Power.begin();
+
+  Wire.begin();
   Serial.begin(115200);
 
-  while (!Serial) {}
-
   sbus_rx.Begin(16, 17);
-  M5.begin();
 
-  M5.Display.setEpdMode(epd_mode_t::epd_fastest);
-  M5.Display.startWrite();
-  M5.Display.setTextSize(2);
+  M5.Lcd.fillScreen(TFT_BLACK);
+
+  //spr.setTextSize(2);
+  spr.setFreeFont(CF_OL24);
+  spr.setColorDepth(8);
+  spr.createSprite(320, 240);
 }
 
 void loop() {
-  M5.update();
+  spr.fillSprite(TFT_BLACK);
 
-  if (sbus_rx.Read()) 
+  if (sbus_rx.Read())
   {
     sbus_data = sbus_rx.ch();
 
+    spr.setTextColor(WHITE); 
+
     for (int8_t i = 0; i < max_ch; i++) {
-      M5.Lcd.setCursor(5, i * 15);
-      M5.Lcd.print("CH");
-      M5.Lcd.print(i);
-      M5.Lcd.print(": ");
-      M5.Lcd.println(perc(sbus_data[i]));
+      spr.setCursor(0, i * 20);
+      spr.printf("CH%d: %d\n", i, perc(sbus_data[i]));
+
       Serial.print(sbus_data[i]);
       Serial.print("\t");
     }
@@ -50,7 +62,14 @@ void loop() {
     Serial.print(sbus_rx.lost_frame());
     Serial.print("\t");
     Serial.println(sbus_rx.failsafe());
-
-    M5.Display.display();
   }
+
+  spr.setTextColor(GREEN); 
+
+  for (int8_t i = 0; i < 4; i++) {
+    spr.setCursor(150, i * 20 ); //+ 8 * 15
+    spr.printf("Enc%d: %d\n", i, ReadEncoder(i));
+  }
+
+  spr.pushSprite(0, 0);
 }

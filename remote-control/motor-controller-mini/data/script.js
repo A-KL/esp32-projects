f52d77dac6db613279b6056ae580beb787dd6e76
DOMINOS\Anatolii.Klots <anatolii.klots@dominos.nl>
2024-02-15 13:54:35 +0100

    better web ui

--- remote-control/motor-controller-mini/data/script.js
+++ remote-control/motor-controller-mini/data/script.js
@@ -1,16 +1,22 @@
-var gateway = `ws://${window.location.hostname}/ws`;
+var gateway = `ws://${location.hostname}:${location.port}/ws`;
 var websocket;
 
-// Init web socket when the page loads
-window.addEventListener('load', onload);
-
-function onload(event) {
+$(function () {
     initWebSocket();
     initUI();
     init_sbus_table();
-}
+});
 
-function getReadings(){
+// Init web socket when the page loads
+//window.addEventListener('load', onload);
+
+//function onload(event) {
+//    initWebSocket();
+//    initUI();
+//    init_sbus_table();
+//}
+
+function getReadings() {
     websocket.send("getReadings");
 }
 
@@ -30,19 +36,15 @@ function onOpen(event) {
 
 // Function that receives the message from the ESP32 with the readings
 function onMessage(event) {
-    console.log(event.data);   
+    console.log(event.data);
     var message = JSON.parse(event.data);
 
-    if (message.hasOwnProperty('sbus_data')) 
-    {
-      var sbus_data = message["sbus_data"];
-      for (var i = 0; i < sbus_data.length; i++)
-      {
-        var cell = document.getElementById("sbus_table_row_" + i.toString() + "_value");
-        cell.innerHTML = sbus_data[i];
-        var meter = document.getElementById("sbus_table_row_" + i.toString() + "_progress");
-        meter.setAttribute("value", sbus_data[i].toString());
-      }
+    if (message.hasOwnProperty('sbus_data')) {
+        var sbus_data = message["sbus_data"];
+        for (var i = 0; i < sbus_data.length; i++) {
+            $("#sbus_table_row_" + i + "_value").html(sbus_data[i]);
+            $("#sbus_table_row_" + i + "_progress").val(sbus_data[i]);
+        }
     }
 
     // for (var i = 0; i < keys.length; i++){
@@ -51,83 +53,60 @@ function onMessage(event) {
     // }
 }
 
-function init_sbus_table() 
-{
-  var table = document.getElementById('sbus_table');
+function init_sbus_table() {
+    for (var i = 0; i < 16; i++) {
+        $("#sbus_table").append(createRow(i));
+    }
+}
 
-  // Clear existing table
-  // while (table.firstChild) {
-  //   table.removeChild(table.firstChild);
-  // }
-  
-  for (var i = 0; i < 16; i++) 
-  {
-    let newRow = document.createElement("tr");
-
-    let statusCell = document.createElement("td");
-    let numberCell = document.createElement("td");
-    let valueCell = document.createElement("td");
-    let progressCell = document.createElement("td");
-
-    let meter = document.createElement("meter");
-    meter.setAttribute("min", "200");
-    meter.setAttribute("max", "1800");
-    meter.setAttribute("id", "sbus_table_row_" + i.toString() + "_progress");
-
-    statusCell.setAttribute("id", "sbus_table_row_" + i.toString() + "_status");
-    valueCell.setAttribute("id", "sbus_table_row_" + i.toString() + "_value");
-
-    progressCell.appendChild(meter);
-
-    numberCell.innerText = (i+1);
-    valueCell.innerText = 0;
-
-    newRow.appendChild(numberCell);
-    newRow.appendChild(valueCell);
-    newRow.appendChild(progressCell);
-
-    table.appendChild(newRow);
-  }
+function createRow(i) {
+    return `
+        <tr>
+            <td><span class="available"></span></td>
+            <td>${(i+1)}</td>
+            <td id="sbus_table_row_${i}_value"></td>
+            <td><meter id="sbus_table_row_${i}_progress" min="200" max="1800"></meter></td>
+        </tr>`;
 }
 
 function initUI() {
-  // document
-  //   .getElementById('button')
-  //   .addEventListener('click', toggle);
+    // document
+    //   .getElementById('button')
+    //   .addEventListener('click', toggle);
 }
 
-var red = document.getElementById("red");
+//var red = document.getElementById("red");
 
-// red.addEventListener("change",() => 
+// red.addEventListener("change",() =>
 // {
 //   console.log('Red :: ' + red.checked);
 //   if(red.checked){
-//     sendMessage("RED_ON"); 
+//     sendMessage("RED_ON");
 //   }else{
-//     sendMessage("RED_OFF"); 
+//     sendMessage("RED_OFF");
 //   }
 // })
 
 // var blue = document.getElementById("blue");
 
-// blue.addEventListener("change",() => 
+// blue.addEventListener("change",() =>
 // {
 //   console.log('Blue :: ' + blue.checked);
 //   if(blue.checked){
-//     sendMessage("BLUE_ON"); 
+//     sendMessage("BLUE_ON");
 //   }else{
-//     sendMessage("BLUE_OFF"); 
+//     sendMessage("BLUE_OFF");
 //   }
 // })
 
 // var green = document.getElementById("green");
 
-// green.addEventListener("change",() => 
+// green.addEventListener("change",() =>
 // {
 //   console.log('Green :: ' + green.checked);
 //   if(green.checked){
-//     sendMessage("GREEN_ON"); 
+//     sendMessage("GREEN_ON");
 //   }else{
-//     sendMessage("GREEN_OFF"); 
+//     sendMessage("GREEN_OFF");
 //   }
 // })

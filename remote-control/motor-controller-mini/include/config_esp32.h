ac6513b67ea0da77f2adc0a0a7082544b331431e
Anatolii Klots <anatolii.klots@gmail.com>
2024-08-03 18:20:36 +0200

    fixing servo pwm

--- remote-control/motor-controller-mini/include/config_esp32.h
+++ remote-control/motor-controller-mini/include/config_esp32.h
@@ -18,11 +18,22 @@
 #define MOTOR_RES       10
 #define MOTOR_DUTY_CYCLE (long)(pow(2, MOTOR_RES) - 1)
 
-#define SERVO_FREQ      50
-#define SERVO_RES       10
 
-#define SERVO_LOW       500
-#define SERVO_HIGH      2000
+#define SERVO_FREQ      50
+#define SERVO_RES       14
+
+// #define SERVO_LOW       500
+// #define SERVO_HIGH      2500
+//#define SERVO_DEFAULT     ((SERVO_LOW+SERVO_HIGH)/2)
+
+#define SERVO_DUTY_CYCLE  ((long)((1<<SERVO_RES)-1))
+
+#define SERVO_LOW       ((long)(SERVO_DUTY_CYCLE * 0.025))
+#define SERVO_HIGH      ((long)(SERVO_DUTY_CYCLE * 0.125))
+
+// #define SERVO_MIN_USEC (100* / SERVO_FREQ)
+// #define SERVO_MAX_USEC (100* / SERVO_FREQ)
+// #define SERVO_PERIOD_USEC (100* / SERVO_FREQ)
 
 #define LEGO_SERVO_LOW  0
 #define LEGO_SERVO_HIGH (long)(pow(2, SERVO_RES) - 1)

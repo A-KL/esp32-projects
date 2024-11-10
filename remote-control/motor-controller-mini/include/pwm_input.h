31b16cc4be01ce49056282aa029267cd65a03167
Anatolii Klots <anatolii.klots@gmail.com>
2024-11-10 22:04:11 +0100

    Adding pwm-input

--- remote-control/motor-controller-mini/include/pwm_input.h
+++ remote-control/motor-controller-mini/include/pwm_input.h
@@ -19,7 +19,7 @@ public:
       startTime = currentTime;
     }
   }
-  
+
   int Result() 
   {
     if (pulses < 2000) {
@@ -52,4 +52,31 @@ inline void pwm_in_deinit() {
     for(auto i=0; i<pwm_inputs_count; i++) {
       detachInterrupt(digitalPinToInterrupt(pwm_input_pins[i]));
   }      
+}
+
+bool pwm_receive(const uint8_t index, int16_t* output) 
+{ 
+  if (pwm_inputs_count <= index) {
+    return false;
+  }
+
+  auto pwm_value = input_pwm[index].Result();
+  auto pwm_detected = pwm_value > INPUT_PWM_ZERO;
+
+  if (pwm_detected) {
+    output[index] = constrain(pwm_value, INPUT_PWM_MIN, INPUT_PWM_MAX);
+  }
+
+  return pwm_detected;
+}
+
+inline int pwm_receive(int16_t* outputs) 
+{
+  int count = 0;
+  for (auto i=0; i<pwm_inputs_count; i++) {
+    if(pwm_receive(i, outputs)) {
+      count++;
+    }
+  }
+  return count;
 }

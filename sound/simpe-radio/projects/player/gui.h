a7e34de1628a7367edc879d55d42283be1e8993c
Anatolii Klots <anatolii.klots@gmail.com>
2024-11-19 16:24:39 +0100

    Implementing back buffer

--- sound/simpe-radio/projects/player/gui.h
+++ sound/simpe-radio/projects/player/gui.h
@@ -5,6 +5,7 @@
 
 #ifdef RM67162_DRIVER
 #include "rm67162.h"
+TFT_eSprite lcd_buffer = TFT_eSprite(&tft);
 #endif
 
 TFT_eCassettePlayer player(&tft, TFT_HEIGHT, TFT_WIDTH, 0, 0);
@@ -16,25 +17,30 @@ void gui_init()
   tft.setSwapBytes(true);
   tft.fillScreen(TFT_BLACK);
 
+#ifdef RM67162_DRIVER
+  lcd_buffer.createSprite(TFT_WIDTH, TFT_HEIGHT);
+  lcd_buffer.setSwapBytes(1);
+  lcd_buffer.fillSprite(TFT_BLACK);
+  player.set_parent(&lcd_buffer);
+
+  lcd_init();
+  lcd_brightness(200);
+#endif
+
   // tft.setFreeFont(&Orbitron_Medium_20);
   player.load_font(NotoSansBold15);
   player.init();
   player.begin();
-
-#ifdef RM67162_DRIVER
-  lcd_init();
-  //lcd_PushColors(0, 0, 240,536, (uint16_t*)tft.getPointer());
-#endif
 }
 
 void gui_update()
 {
     // ovr_label.foreground_color = right_pb.value > 1000 ? TFT_RED : TFT_DARK_DARK_GRAY;
+    player.update();
 
-    // gui_pb_update(left_pb);
-    // gui_pb_update(right_pb);
-
-    // gui_label_update(ovr_label);
+#ifdef RM67162_DRIVER
+    lcd_PushColors(0, 0, TFT_WIDTH, TFT_HEIGHT, (uint16_t*)lcd_buffer.getPointer());
+#endif
 }
 
 void gui_update_task(void *arg)  

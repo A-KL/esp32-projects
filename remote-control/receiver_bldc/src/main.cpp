9b00507e4c87427649750f14f31f476137470130
Anatolii Klots <anatolii.klots@gmail.com>
2025-01-08 19:09:55 +0100

    More implementation

--- remote-control/receiver_bldc/src/main.cpp
+++ remote-control/receiver_bldc/src/main.cpp
@@ -11,40 +11,45 @@
 
 #include <ps_input.h>
 
-driver_strategy_t<int16_t> left_value(-255, 255, 0);
-driver_strategy_t<int16_t> right_value(-255, 255, 0);
+static const int16_t Power_Min = -255;
+static const int16_t Power_Max = 255;
+static const int16_t dead_zone = 18;
+
+static driver_strategy_t<int16_t> left_value(Power_Min, Power_Max, 0);
+static driver_strategy_t<int16_t> right_value(Power_Min, Power_Max, 0);
+
+static inline int16_t filter_dead_zone(int16_t value)
+{
+  return (abs(value) < dead_zone) ? 0 : value;
+}
+
+static inline void write(int16_t power, int16_t steer)
+{
+  static int16_t left_speed = constrain(power - steer, Power_Min, Power_Max);
+  static int16_t right_speed = constrain(power + steer, Power_Min, Power_Max);
+
+  left_value.write(left_speed);
+  right_value.write(right_speed);
+}
 
 static void on_ps3_input(int8_t lx, int8_t ly, int8_t rx, int8_t ry, int8_t l2, int8_t r2) 
 {
-    const static int dead_zone = 20;
-    
-    auto power = r2 - l2;
-    auto steer = - lx;
+  auto power = r2 - l2;
+  auto steer = - lx;
 
-    if (abs(power) < dead_zone) {
-      power = 0;
-    }
-    if (abs(steer) < dead_zone) {
-      steer = 0;
-    }
+  power = filter_dead_zone(power);
+  steer = filter_dead_zone(steer);
 
-    static int16_t left_speed = constrain(power - steer, -255, 255);
-    static int16_t right_speed = constrain(power + steer, -255, 255);
-
-  // left_value.write();
-  // right_value.write();
+  write(power, steer);
 }
 
 void setup() {
   Serial.begin(115200);
 
-  delay(5000);
+  delay(3000);
 
   ps_attach(on_ps3_input);
   ps_init();
-
-  left_value.write(-255);
-  right_value.write(255);
 
   motors_init();
 

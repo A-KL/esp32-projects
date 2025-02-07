153ecc2ee08f994ae51c6cf1e99ed1c7b027e0bc
Anatolii Klots <A-KL@users.noreply.github.com>
2025-02-07 22:14:22 +0100

    New project for BLDC driver (#7)
    * Branch
    
    * More refactoring
    
    * Clean up
    
    * More fixes
    
    * adding brake pin
    
    ---------
    
    Co-authored-by: Anatolii Klots <anatolii.klots@gmail.com>

--- remote-control/receiver_bldc/include/motor_output.h
+++ remote-control/receiver_bldc/include/motor_output.h
@@ -2,62 +2,94 @@
 
 #include <Arduino.h>
 
-#ifndef MOTOR_INPUT_DEAD_ZONE
-#define MOTOR_INPUT_DEAD_ZONE 10
-#endif
-
-#ifndef MOTOR_PWM_FQC
-#define MOTOR_PWM_FQC         10000 // Hz
-#endif
-
-#ifndef MOTOR_PWM_RESOLUTION
-#define MOTOR_PWM_RESOLUTION  8 // Bit
-#endif
-
-#ifndef INPUT_MOTOR_MAX_LIMIT
-#define INPUT_MOTOR_MAX_LIMIT 1
-#endif
-
-const static int motor_duty_cycle = (255 * INPUT_MOTOR_MAX_LIMIT); // with 8 (0-255), 12 (0-4095), or 16 (0-65535) bit resolution
-
-// Utils
-
-inline bool near_zero(const int value) {
-  return (abs(value) < MOTOR_INPUT_DEAD_ZONE);
-}
-
-// Run motors
-
-void motor_write_a_en(const uint8_t pin_a, const uint8_t channel_en, const int16_t speed) 
+class motor_driver_t
 {
-  if (near_zero(speed)) {
-    ledcWrite(channel_en, 0);
-  }
-  else {
-    digitalWrite(pin_a, speed > 0 ? HIGH : LOW);
-    ledcWrite(channel_en, abs(speed));
-  }
-}
+  public:
+    motor_driver_t(
+      const int16_t pin_direction, 
+      const int16_t pin_en, 
+      const int16_t pin_brake,
+      const int16_t pin_stop,
+      const uint64_t frequency = 10000, 
+      const uint16_t resolution = 10, 
+      const uint16_t dead_zone = 10)
+    : _pin_direction(pin_direction), 
+      _pin_en(pin_en), 
+      _pin_brake(pin_brake),
+      _pin_stop(pin_stop),
+      _frequency(frequency), 
+      _resolution(resolution), 
+      _dead_zone(dead_zone)
+    {}
 
-template<short TMin, short TMax>
-void motor_write_a_en(const uint8_t pin_a, const uint8_t channel_en, const int16_t speed)
-{
-  motor_write_a_en(pin_a, channel_en, map(constrain(speed, TMin, TMax), TMin, TMax, -motor_duty_cycle, motor_duty_cycle));
-}
+    motor_driver_t& init()
+    {
+      if (_pin_en_ch == -1) {
+        _pin_en_ch = _channels++;
+      }
 
-// Init motors
+      ledcSetup(_pin_en_ch, _frequency, _resolution);
+      ledcAttachPin(_pin_en, _pin_en_ch);   
 
-void motor_init_a_en(const uint8_t pin_a, const uint8_t pin_en, const uint8_t channel_en = 0)
-{
-  pinMode(pin_a, OUTPUT);
+      pinMode(_pin_direction, OUTPUT);
+      // pinMode(_pin_brake, OUTPUT);
+      // pinMode(_pin_stop, OUTPUT);
 
-  ledcSetup(channel_en, MOTOR_PWM_FQC, MOTOR_PWM_RESOLUTION);
-  ledcAttachPin(pin_en, channel_en);
-}
+      return *this;
+    }
 
-void motors_init()
-{
-    for (auto i = 0; i < motors_count; i++) {
-    motor_init_a_en(motor_dir[i], motor_speed[i], motor_pwm_ch[i]);
-  }
-}
+    void stop(bool stop = true)
+    {
+        _stop = stop;
+        digitalWrite(_pin_stop, _stop);
+    }
+
+    void brake(bool brake = true) const
+    {
+        digitalWrite(_pin_brake, brake);
+    }
+
+    void write(int16_t speed) const
+    {
+        if (near_zero(speed)) 
+        {
+          ledcWrite(_pin_en_ch, 0);
+        }
+        else 
+        {
+          digitalWrite(_pin_direction, speed > 0 ? HIGH : LOW);
+          ledcWrite(_pin_en_ch, abs(speed));
+        }
+    }
+
+  private:
+    bool _stop = false;
+
+    static uint8_t _channels;
+
+    const int16_t _pin_stop;
+    const int16_t _pin_brake;
+
+    const int16_t _pin_direction;
+    const int16_t _pin_en;
+
+    int16_t _pin_en_ch = -1;
+
+    const uint64_t _frequency;
+    const uint16_t _resolution;
+    const uint16_t _dead_zone;
+
+    inline bool near_zero(const int16_t value) const {
+      return (abs(value) < _dead_zone);
+    }
+};
+
+uint8_t motor_driver_t::_channels = 0;
+
+// const static int motor_duty_cycle = (255 * INPUT_MOTOR_MAX_LIMIT); // with 8 (0-255), 12 (0-4095), or 16 (0-65535) bit resolution
+
+// template<short TMin, short TMax>
+// void motor_write_a_en(const uint8_t pin_a, const uint8_t channel_en, const int16_t speed)
+// {
+//   motor_write_a_en(pin_a, channel_en, map(constrain(speed, TMin, TMax), TMin, TMax, -motor_duty_cycle, motor_duty_cycle));
+// }
--- remote-control/receiver_bldc/src/main.cpp
+++ remote-control/receiver_bldc/src/main.cpp
@@ -1,22 +1,43 @@
 #include <Arduino.h>
 
+#ifndef MOTOR_INPUT_DEAD_ZONE
+#define MOTOR_INPUT_DEAD_ZONE 10
+#endif
+
+#ifndef MOTOR_PWM_FQC
+#define MOTOR_PWM_FQC         15000 // Hz
+#endif
+
+#ifndef MOTOR_PWM_RESOLUTION
+#define MOTOR_PWM_RESOLUTION  8 // Bit
+#endif
+
+#ifndef INPUT_MOTOR_MAX_LIMIT
+#define INPUT_MOTOR_MAX_LIMIT 1
+#endif
+
 #ifndef CONFIG_FILE
 #error "Config file was not defined"
 #endif
 #include CONFIG_FILE
 
-#include <driver.h>
+#include <sbus.h>
 #include <driver_strategy_t.h>
 #include <motor_output.h>
-
 #include <ps_input.h>
 
 static const int16_t Power_Min = -255;
 static const int16_t Power_Max = 255;
-static const int16_t dead_zone = 18;
+static const int16_t dead_zone = 20;
+
+static motor_driver_t motor_left(25, 33, -1, -1, MOTOR_PWM_FQC, MOTOR_PWM_RESOLUTION);
+static motor_driver_t motor_right(19, 21, 18, -1, MOTOR_PWM_FQC, MOTOR_PWM_RESOLUTION);
 
 static driver_strategy_t<int16_t> left_value(Power_Min, Power_Max, 0);
 static driver_strategy_t<int16_t> right_value(Power_Min, Power_Max, 0);
+
+bfs::SbusTx sbus_tx(sbus_serial, sbus_rx_tx_pins[0], sbus_rx_tx_pins[1], true);
+bfs::SbusData sbus_data;
 
 static inline int16_t filter_dead_zone(const int16_t value)
 {
@@ -37,7 +58,7 @@ static void on_ps3_lost()
   write(0, 0);
 }
 
-static void on_ps3_input(int16_t lx, int16_t ly, int16_t rx, int16_t ry, int16_t l2, int16_t r2) 
+static void on_ps3_input(int16_t lx, int16_t ly, int16_t rx, int16_t ry, int16_t l2, int16_t r2, int16_t triangle, int16_t cross) 
 {
   auto power = r2 - l2;
   auto steer = - lx;
@@ -51,7 +72,8 @@ static void on_ps3_input(int16_t lx, int16_t ly, int16_t rx, int16_t ry, int16_t
   write(power, steer);
 }
 
-void setup() {
+void setup() 
+{
   Serial.begin(115200);
 
   delay(3000);
@@ -59,19 +81,22 @@ void setup() {
   ps_attach(on_ps3_input, on_ps3_lost);
   ps_init();
 
-  motors_init();
+  motor_left.init();
+  motor_right.init();
 
   // wifi_init();
   // now_init();
 }
 
-void loop() {
+void loop() 
+{
   auto left = left_value.read();
   auto right = right_value.read();
 
   log_d("Speed:\t%d\t%d", left, right);
+
   delay(50);
 
-  motor_write_a_en(motor_dir[0], motor_pwm_ch[0], left);
-  motor_write_a_en(motor_dir[1], motor_pwm_ch[1], right);
+  motor_left.write(left);
+  motor_right.write(right);
 }

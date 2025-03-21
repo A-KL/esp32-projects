180e19797f1fcb3a0f3d3ba5e175d172e87e0d0e
Anatolii Klots <anatolii.klots@gmail.com>
2025-03-21 13:27:48 +0100

    Adding s2

--- sound/simpe-radio/projects/vu-meter/main.cpp
+++ sound/simpe-radio/projects/vu-meter/main.cpp
@@ -17,15 +17,13 @@ MultiOutput decoded_out;
 MultiOutput encoded_out;
 
 I2SStream i2s;
-VuMeter<int16_t> vu(0.1);
+VuMeter<int16_t> vu(AUDIO_VU_RATIO);
 NumberFormatConverterStream nfc(decoded_out);
 EncodedAudioStream decoder(&nfc, new MP3DecoderHelix());
 
 MetaDataOutput metadata; // final output of metadata
 
 StreamCopy copier(encoded_out, url);
-
-const int output_format = 16;
 
 void print_metadata(MetaDataType type, const char* str, int len){
   Serial.printf("==> %s: %s\r\n", toStr(type), str);
@@ -42,10 +40,10 @@ void setup(){
   gui_init();
   gui_set_input((int)1);
   
-  AudioLogger::instance().begin(Serial, AudioLogger::Info);
+  AudioLogger::instance().begin(Serial, AudioLogger::Warning);
 
   //nfc.addNotifyAudioChange(i2s);
-  nfc.begin(16, output_format);
+  nfc.begin(16, I2S_BPS);
 
   url.begin("http://stream.srg-ssr.ch/m/rsj/mp3_128","audio/mp3");
 
@@ -53,17 +51,16 @@ void setup(){
   config.pin_ws = I2S_WS;
   config.pin_bck = I2S_BCK;
   config.pin_data = I2S_SD;
-  config.bits_per_sample = output_format;
   config.is_master = true;
 
-  encoded_out.add(metadata);
+  //encoded_out.add(metadata);
   encoded_out.add(decoder);
 
   decoded_out.add(vu);
   decoded_out.add(i2s);
 
-  metadata.setCallback(print_metadata);
-  metadata.begin(url.httpRequest());
+  //metadata.setCallback(print_metadata);
+ // metadata.begin(url.httpRequest());
 
   i2s.begin(config);
   decoder.begin();
@@ -73,7 +70,6 @@ void setup(){
 
 void loop(){
   copier.copy();
-
   left_pb.value = vu.value_left();
   right_pb.value = vu.value_right();
 }

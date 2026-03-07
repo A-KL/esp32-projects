263a69d0686141dc4e9c1190ec05c35cb92522b1
Anatolii Klots <anatolii.klots@gmail.com>
2026-03-07 22:59:05 +0100

    bldc config

--- remote-control/motor-controller-mini/include/driver_config_bldc.h
+++ remote-control/motor-controller-mini/include/driver_config_bldc.h
@@ -8,10 +8,10 @@
 const uint8_t adc_input_pins[] = { };
 
 /* PWM */
-const uint8_t pwm_input_pins[] = { };
+const uint8_t pwm_input_pins[] = { 23, 22 };
 
 /* SBUS */
-const uint8_t sbus_rx_tx_pins[] = { 16, 17 };
+const uint8_t sbus_rx_tx_pins[] = { }; //16, 17;
 const auto sbus_serial = &Serial1;
 
 /* PS */
@@ -22,18 +22,13 @@ const char* ps_controller_mac = "B8:27:EB:DF:B3:FF";
 /* Servos */
 const uint8_t servos_pins[] = { };
 
+const bool servos_inverted[] = {};
+
 /* LEGO */
 lego_servo_t lego_servos[] = { };
 
 /* Motors */
 motor_config_t motors [] = {
-    { motor_mode_t::dir_en, { 25, -1, 33, -1, -1, 0 } }, //a, b, en, pwm_a, pwm_b, pwm_en
-    { motor_mode_t::dir_en, { 26, -1, 27, -1, -1, 1 } }  //b, b, en, pwm_a, pwm_b, pwm_en
+    { dc_mode_t::dir_en, { 15, -1, 04, -1, -1, 0 } }, //a, b, en, pwm_a, pwm_b, pwm_en
+    { dc_mode_t::dir_en, { 12, -1, 14, -1, -1, 1 } }  //b, b, en, pwm_a, pwm_b, pwm_en
 };
-
-/* Misc */
-
-const uint8_t switch_input_button = 4;
-
-// const uint8_t led_output = 8;
-// const short rgb_output = -1;

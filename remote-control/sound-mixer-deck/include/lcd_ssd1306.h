8ec1522f76e6ffb4fa2476de826c0977aa1e59e8
Anatolii <anatolii.klots@gmail.com>
2026-03-23 17:22:13 +0100

    More code

--- remote-control/sound-mixer-deck/include/lcd_ssd1306.h
+++ remote-control/sound-mixer-deck/include/lcd_ssd1306.h
@@ -6,7 +6,6 @@
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
-
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
--- remote-control/sound-mixer-deck/v2/main.cpp
+++ remote-control/sound-mixer-deck/v2/main.cpp
@@ -3,28 +3,139 @@
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7789.h>
-#include <Fonts/FreeSansBold18pt7b.h> // A custom font
+#include <Fonts/FreeSansBold18pt7b.h>
+#include <Fonts/FreeSansBold9pt7b.h>
+#include <Fonts/FreeSans9pt7b.h>
+
+#include "parsing.h"
 
 #define TFT_CS        10
 #define TFT_RST        9 // Or set to -1 and connect to Arduino RESET pin
 #define TFT_DC         8
 
-Adafruit_ST7789 tft(TFT_CS, TFT_DC, TFT_RST);
+#define SCREEN_WIDTH 284
+#define SCREEN_HEIGHT 76
 
+Adafruit_ST7789 display(TFT_CS, TFT_DC, TFT_RST);
+
+static long long last_received = millis();
+static bool no_signal = false;
+
+void draw_tile(const char* main_text, const char* sub_text, uint16_t color, int x, int y, int width = 70, int height = 76)
+{
+  display.fillRect(x+2, y+2, width-3*2, height-3*2, ST77XX_BLACK);
+  display.flush();
+
+  // FPS label
+  display.setTextColor(color);
+  display.setFont(&FreeSans9pt7b);
+  display.setCursor(x + 15, 12);
+  display.print("FPS");
+
+  // FPS wattage
+  display.setTextColor(ST77XX_WHITE);
+  display.setCursor(x + 15, 70);
+  display.print(sub_text);
+  
+  // FPS value
+  display.setTextColor(color);
+  display.setFont(&FreeSansBold18pt7b);
+  display.setCursor(x + 15, 50);
+  display.print(main_text);
+}
 
 void setup() {
   Serial.begin(115200);
 
-  tft.init(76, 284); // Initialize display with custom dimensions and SPI mode
+  display.init(SCREEN_HEIGHT, SCREEN_WIDTH); // Initialize display with custom dimensions and SPI mode
+  display.invertDisplay(false);
+  display.setRotation(1);
+  // display.setTextSize(1);
+  display.setTextWrap(false);
+  display.setTextColor(ST77XX_WHITE);
 
-  tft.setFont(&FreeSansBold18pt7b);
-  tft.setTextSize(1);
-  tft.setTextWrap(false);
+  display.fillScreen(ST77XX_BLACK);
 
-  tft.fillScreen(ST77XX_RED);
-  tft.setCursor(0, 0);
-  tft.println("Hello, World!");
+  auto x = 0;
+
+  display.drawRect(x      , 0, 70, 76, ST77XX_YELLOW);
+  display.drawRect(x+1    , 1, 68, 74, ST77XX_YELLOW);
+
+  display.drawRect(x += 71, 0, 70, 76, ST77XX_RED);
+  display.drawRect(x += 71, 0, 70, 76, ST77XX_BLUE);
+  display.drawRect(x += 71, 0, 70, 76, ST77XX_MAGENTA);
+
+  // FPS label
+  display.setTextColor(ST77XX_YELLOW);
+  display.setCursor(15, 12);
+  display.setFont(&FreeSans9pt7b);
+  display.println("FPS");
+
+  // FPS wattage
+  display.setTextColor(ST77XX_WHITE);
+  display.setCursor(15, 70);
+  display.println("150w");
+  
+  // FPS value
+  display.setTextColor(ST77XX_YELLOW);
+  display.setCursor(15, 50);
+  display.setFont(&FreeSansBold18pt7b);
+  display.println("60");
 }
 
 void loop() {
+   if (Serial.available()) {
+    auto command = Serial.readStringUntil('\r');
+    
+    // display.setFont();
+    // display.setTextSize(1);
+    // display.setTextColor(ST77XX_WHITE);
+    // display.setCursor(0, 2);
+    // display.fillScreen(ST77XX_BLACK);
+
+    String id;
+    String property;
+    String value;
+
+    // CPU
+    command = parse_input(command, id, property, value);
+    // display.print(id);
+    // display.print(": ");
+    // display.print(value);
+    // display.print(" | ");
+
+    command = parse_input(command, id, property, value);
+    // display.println(value);
+
+    // RAM
+    command = parse_input(command, id, property, value);
+    // display.print(id);
+    // display.print(": ");
+    // display.println(value);
+
+    // GPU
+    command = parse_input(command, id, property, value);
+    // display.print(id);
+    // display.print(": ");
+    // display.print(value);
+    // display.print(" | ");
+
+    draw_tile(value.c_str(), "150w", ST77XX_RED, 0, 0);
+
+    // command = parse_input(command, id, property, value);
+    // display.println(value);
+
+    //display.flush();
+
+    last_received = millis();
+    no_signal = false;
+  }
+
+  if ((millis() - last_received) > 1000 && !no_signal) {
+    no_signal = true;
+   // display.fillScreen(ST77XX_BLACK);
+    display.setFont(&FreeSansBold18pt7b);
+    display.setCursor(0, 76);
+    //display.println("NO SIGNAL");
+  }
 }
